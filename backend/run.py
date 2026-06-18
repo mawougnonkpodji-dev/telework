46acc759fc60f6ee94d61be88bc60af64f0311a8
mawougnonkpodji-dev <mawougnonkpodji@gmail.com>
2026-06-18 11:03:57 +0100

    Avjjdkdk

--- backend/run.py
+++ backend/run.py
@@ -13,7 +13,7 @@ app = create_app(os.getenv("FLASK_ENV", "development"))
 if __name__ == "__main__":
     socketio.run(
         app,
-        debug=True,
+        debug=False,
         port=3001,
-        use_reloader=True,
+        use_reloader=False,
     )
